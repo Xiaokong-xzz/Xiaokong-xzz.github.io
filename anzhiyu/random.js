f11e03fbd0da7b0f81d41d01808d510b149c02c3
kong <767607252@qq.com>
2026-03-27 15:05:49 +0800

    Site updated: 2026-03-27 15:05:48

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/03/26/博客/","2026/03/25/hello-world/"];function toRandomPost(){
+var posts=["2026/03/25/hello-world/","2026/03/26/博客/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
