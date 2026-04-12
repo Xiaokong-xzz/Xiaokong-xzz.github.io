72570d3ae662f9d6057fbad1014f59285ecf7a7e
kong <767607252@qq.com>
2026-04-12 21:50:12 +0800

    Site updated: 2026-04-12 21:50:11

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/03/30/剪辑分享/","2026/03/26/博客/","2026/04/10/typora激活分享/"];function toRandomPost(){
+var posts=["2026/03/30/剪辑分享/","2026/03/26/博客/","2026/04/10/typora激活分享/","2026/04/10/避雷贴（山东省淄博市）/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
